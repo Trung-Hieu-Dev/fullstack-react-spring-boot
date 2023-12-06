a30388142a4b4649a58531b9c077ff06aae79471
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-06 14:57:40 +0700

    Explore Top Books Component

--- library-app/fontend/src/App.tsx
+++ library-app/fontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./layouts/NavbarAndFooter/NavBar";
+import ExploreTopBooks from "./layouts/HomePage/ExploreTopBooks";
 
 function App() {
     return (
-        <NavBar />
+        <div>
+            <NavBar/>
+            <ExploreTopBooks/>
+        </div>
     );
 }
 
