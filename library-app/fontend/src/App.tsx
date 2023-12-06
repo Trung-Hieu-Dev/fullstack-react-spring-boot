8941d32afb4f889da5b516e6cd4fa372501cb891
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-06 15:51:33 +0700

    Carousel Component

--- library-app/fontend/src/App.tsx
+++ library-app/fontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import NavBar from "./layouts/NavbarAndFooter/NavBar";
 import ExploreTopBooks from "./layouts/HomePage/ExploreTopBooks";
+import Carousel from "./layouts/HomePage/Carousel";
 
 function App() {
     return (
         <div>
             <NavBar/>
             <ExploreTopBooks/>
+            <Carousel />
         </div>
     );
 }
