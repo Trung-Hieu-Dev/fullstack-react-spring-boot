608c6a5e453e224d068cae3b55bd4cd771dd525c
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-06 18:34:46 +0700

    Home Page Clean Up

--- library-app/fontend/src/App.tsx
+++ library-app/fontend/src/App.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./layouts/NavbarAndFooter/NavBar";
-import ExploreTopBooks from "./layouts/HomePage/ExploreTopBooks";
-import Carousel from "./layouts/HomePage/Carousel";
-import Heros from "./layouts/HomePage/Heros";
-import LibraryServices from "./layouts/HomePage/LibraryServices";
 import Footer from "./layouts/NavbarAndFooter/Footer";
+import HomePage from "./layouts/HomePage/HomePage";
 
 function App() {
     return (
         <div>
             <NavBar/>
-            <ExploreTopBooks/>
-            <Carousel />
-            <Heros />
-            <LibraryServices />
-            <Footer />
+            <HomePage/>
+            <Footer/>
         </div>
     );
 }
